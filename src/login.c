e40d36cd2ace1f83ecccb4c7c7999c167ad45729
WiseLook <1656438881@qq.com>
2025-01-10 13:32:46 +0800

    Refactor: optimize login.

--- src/login.c
+++ src/login.c
@@ -11,23 +11,26 @@ void login(void)
     while (1)
     {
         printf("\033[H\033[J"); // 清屏
-        printf("请输入用户名：\n");
+        puts("=================================");
+        puts("   欢迎使用实验室预约管理系统！");
+        puts("=================================");
+        printf("请输入用户名：");
         scanf(" %s", username);
         if (strcmp(username, USERNAME) == 0)
         {
-            printf("请输入密码：\n");
+            printf("请输入密码：");
             scanf(" %s", password);
             if (strcmp(password, PASSWORD) == 0)
             {
-                printf("登录成功！\n");
+                puts("登录成功！");
                 Beep(1150, 350);
                 Sleep(350);
                 break;
             }
-            printf("密码错误！请重试\n");
+            puts("密码错误！请重试");
         }
         else
-            printf("不存在该用户！请重试\n");
+            puts("不存在该用户！请重试");
         Beep(500, 200);
     }
 }
